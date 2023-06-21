7d266916e27eab998c68317f6c562b63f2a65e83
goeych <110361567+goeych@users.noreply.github.com>
2023-06-21 17:00:51 +0800

    Add files via upload

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,8 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('products/', views.products),
-    path('customer/', views.customer),
+    path('', views.home,name='home'),
+    path('products/', views.products,name='products'),
+    path('customers/', views.customers,name='customers'),
     path('customer/<str:pk>/',views.customer,name='customer'),
+    path('createorder/<str:pk>/',views.createOrder,name='createOrder'),
+    path('updateorder/<str:pk>/',views.updateOrder,name="updateOrder"),
+    path('deleteorder/<str:pk>/',views.deleteOrder,name="deleteOrder"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
-
+from django.forms import inlineformset_factory
 from .models import *
+from .forms import OrderForm
+from .filters import OrderFilter
 
 # Create your views here.
 
@@ -13,7 +15,7 @@ def home(request):
     total_orders = orders.count()
 
     delivered = orders.filter(status='Delivered').count()
-    pending = orders.filter(status='pending').count()
+    pending = orders.filter(status='Pending').count()
 
     context={'orders':orders,'customers':customers,
              'total_customers':total_customers,
@@ -29,7 +31,7 @@ def products(request):
     context={'products':products}
     return render(request,'accounts/products.html',context)
 
-def customer(request):
+def customers(request):
     context={}
     return render(request,'accounts/customer.html',context)
 
@@ -37,9 +39,70 @@ def customer(request,pk):
     customer = Customer.objects.get(id=pk)
 
     orders = customer.order_set.all()
+    orders_count = orders.count()
 
-    context={'customer':customer,
-             'orders':orders}
+    myFilter = OrderFilter(request.GET, queryset=orders)
+    print('myFilter:',myFilter)
+    orders = myFilter.qs
+
+    context={'customer':customer,'orders':orders,
+             'orders_count':orders_count,'myFilter':myFilter,
+             }
     return render(request,'accounts/customer.html',context)
+
+def createOrder(request,pk):
+    OrderFormSet = inlineformset_factory(Customer,Order,fields=('product','status'),extra=2)
+    #extra 10 is list more 10 rows in the form.
+    
+    customer = Customer.objects.get(id=pk)
+    formset = OrderFormSet(queryset=Order.objects.none(),instance=customer)
+    #queryset=Order.objects.none() no record will show in form.
+    
+    #form = OrderForm(initial={'customer':customer})
+    if request.method == 'POST':
+        formset = OrderFormSet(request.POST,instance=customer)
+        if formset.is_valid():
+            formset.save()
+            return redirect('home')
+
+
+    #form = OrderForm(initial={'customer':customer})
+    #if request.method == 'POST':
+        #form = OrderForm(request.POST)
+        #if form.is_valid():
+            #form.save()
+            #return redirect('home')
+    
+
+    #context={'form':form}
+    context={'formset':formset}
+    return render(request,'accounts/createorder.html',context)
+
+def updateOrder(request,pk):
+
+    order = Order.objects.get(id=pk)
+
+    form = OrderForm(instance = order)
+    if request.method == 'POST':
+        form = OrderForm(request.POST,instance = order)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context={'form':form}
+    return render(request,'accounts/createorder.html',context)
+
+
+def deleteOrder(request,pk):
+    order = Order.objects.get(id=pk)
+
+    if request.method == 'POST':
+        order.delete()
+        return redirect('home')
+
+    context={'order':order}
+    return render(request,'accounts/deleteorder.html',context)
+
+    
     
 
