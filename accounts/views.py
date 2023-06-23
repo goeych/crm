6b5ddfc05a0c4a39c7e20eaef9b7bbf1066a343f
goeych <110361567+goeych@users.noreply.github.com>
2023-06-23 17:00:49 +0800

    Add files via upload

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.forms import inlineformset_factory
+from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 from .models import *
 from .forms import OrderForm,CreateUserForm
@@ -17,15 +19,33 @@ def registerPage(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request,"Account was created for " + user)
+            #return redirect('login')
 
     context={'form':form}
     return render(request,'accounts/register.html',context)
 
 def loginPage(request):
 
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request,username=username,password=password)
+
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.info(request,'Username or password is incorrect')
+
     context={'products':products}
     return render(request,'accounts/login.html',context)
-    
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
 
 def home(request):
     orders = Order.objects.all()
