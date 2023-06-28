313114b0e0c34367ec77567b08b14ce911f24b89
goeych <110361567+goeych@users.noreply.github.com>
2023-06-28 14:41:54 +0800

    Add files via upload

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -17,4 +18,22 @@ urlpatterns = [
     path('createorder/<str:pk>/',views.createOrder,name='createOrder'),
     path('updateorder/<str:pk>/',views.updateOrder,name="updateOrder"),
     path('deleteorder/<str:pk>/',views.deleteOrder,name="deleteOrder"),
+
+    path('reset_password/',
+         auth_views.PasswordResetView.as_view(template_name="accounts/password_reset.html"),
+         name="reset_password"),
+    
+    path('reset_password_sent/',
+         auth_views.PasswordResetDoneView.as_view(template_name="accounts/password_reset_sent.html"),
+         name="password_reset_done"),
+    
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name="password_reset_form.html"),
+         name="password_reset_confirm"),
+
+    
+    path('reset_password_complete/',
+         auth_views.PasswordResetCompleteView.as_view(template_name="password_reset_done.html"),
+         name="password_reset_complete"),
+
 ]
